4a3b0b0c6fc5d2e4dcbe9e3e5e1f32a87f11fb68
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-09-05 01:44:02 +0600

    crate Properties class for photo upload

---
+++ src/main/java/com/ewallet/ewalletagent/attachment/config/Properties.java
@@ -0,0 +1,8 @@
+package com.ewallet.ewalletagent.attachment.config;
+
+public class Properties {
+    public static final int MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
+    public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/ewallet/";
+    public static final String Attachment_FOLDER= "attachment/";
+    public static final String NID_CARD_FOLDER= "nid_card/";
+}
